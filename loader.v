993a61db8dfc1a4271bd94cb16f3be65fbca4bbd
antono3 <riddles-yt@gmx.de>
2026-09-07 15:21:03 +0200

    Expose Volk loader lifecycle (#5)
    Co-authored-by: Anton Oreskin <antono2@web.de>

---
+++ loader.v
@@ -0,0 +1,19 @@
+module vulkan
+
+// initialize_loader loads the Vulkan loader and populates the global commands.
+// Call this before create_instance().
+pub fn initialize_loader() Result {
+	return C.volkInitialize()
+}
+
+// load_instance_commands populates commands that are scoped to an instance.
+// Call this immediately after successfully creating an instance.
+pub fn load_instance_commands(instance Instance) {
+	C.volkLoadInstance(instance)
+}
+
+// load_device_commands populates commands that are scoped to a device.
+// Call this immediately after successfully creating a device.
+pub fn load_device_commands(device Device) {
+	C.volkLoadDevice(device)
+}
