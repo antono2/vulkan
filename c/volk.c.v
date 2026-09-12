828dc995ff7488405f42368f2265952e080af8d8
Anton Oreskin <antono2@web.de>
2026-09-12 11:09:56 +0200

    Keep Volk include compatible with V 0.5.2

--- c/volk.c.v
+++ c/volk.c.v
@@ -9,11 +9,5 @@ module c
 #flag windows -I$env('VULKAN_SDK')/Include/volk
 #define VK_NO_PROTOTYPES
 #define VOLK_IMPLEMENTATION
-#if __has_include(<volk.h>)
 #include <volk.h>
-#elif __has_include(<volk/volk.h>)
-#include <volk/volk.h>
-#else
-#error "Volk headers not found; run `v run setup.vsh` from antono2.vulkan"
-#endif
 #include "loader_bridge.h"
